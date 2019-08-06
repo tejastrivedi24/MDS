8f70a28c0aff03570866791a2840b9f6a18c648e
tejastrivedi24 <37214962+tejastrivedi24@users.noreply.github.com>
2019-08-06 23:26:12 +0530

    Add files via upload

---
+++ MDS/mds/ind.php
@@ -0,0 +1,523 @@
+<?php
+$error = NULL;
+
+if(isset($_POST['Signup'])){
+  //Get data
+  $n1 = $_POST['fname'];
+  $n2 = $_POST['lname'];
+  $n3 = $_POST['email'];
+  $n4 = $_POST['psw1'];
+  $n5 = $_POST['psw2'];
+  $n6 = $_POST['contact'];
+
+  if(strlen($psw1) < 5){
+    $error = "<p>Your Password must be at least 5 characters</p>";
+  }
+  elseif($psw2 != $psw1){
+    $error = "<p>Oopps!! Your passwords do not match</p>";
+  }
+  else{
+    //Form is valid
+    //Connect to the database
+    $mysqli = NEW MySQLi('localhost','root','','mds');
+
+    $n1 = $mysqli->real_escape_string($fname);
+    $n2 = $mysqli->real_escape_string($lname);
+    $n3 = $mysqli->real_escape_string($email);
+    $n4 = $mysqli->real_escape_string($psw1);
+    $n5 = $mysqli->real_escape_string($psw2);
+    $n6 = $mysqli->real_escape_string($contact);
+
+    $insert = $mysqli->query("INSERT INTO registered_user(Fname,Lname,Email,Password,Contact) VALUES('$n1','$n2','$n3','$n4','$n6')");
+
+    if($insert){
+      echo "Success";
+    }else{
+      echo $mysqli->error;
+
+    }
+    }
+  
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="shortcut icon" href="https://www.yummytiffins.com/img/favicon.ico" />
+    <link rel="apple-touch-icon" href="https://www.yummytiffins.com/img/favicon.png" />
+    <!--css-->
+    <link rel="stylesheet" href="https://www.yummytiffins.com/css/combine.css"/>
+        <link rel="stylesheet" href="https://www.yummytiffins.com/css/slick.css"/>
+    <link rel="stylesheet" href="https://www.yummytiffins.com/css/slick-theme.css"/>
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+    <meta content="noindex, nofollow" name="robots">
+<link href="css/showhide.css" rel="stylesheet">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="js/showhide.js"></script>
+    <style>
+    .contain{
+  background-color:#fff;
+}
+    .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+.fa-anchor,.fa-coffee {font-size:200px}
+ 
+    
+    /*assign full width inputs*/ 
+    input[type=text], 
+    input[type=password] { 
+        width: 49%; 
+        padding: 10px 20px; 
+        margin: 8px 0; 
+        display: inline-block; 
+        border: 1px solid #ccc; 
+        box-sizing: border-box; 
+    }
+
+    .hand{font-family: "Montserrat", sans-serif;
+    color:fff;
+    margin: -3%;}
+
+     
+    
+    /*set a style for the buttons*/ 
+    button { 
+        background-color: #de4949; 
+        color: white; 
+        padding: 5px 18px; 
+        margin: 18px 0; 
+        border: none; 
+        cursor: pointer; 
+        width: 25%; 
+    } 
+    
+    /* set a hover effect for the button*/ 
+    button:hover { 
+        opacity: 0.5; 
+    } 
+    
+    /*set extra style for the cancel button*/ 
+    .cancelbtn { 
+        width: auto; 
+        padding: 5px 18px; 
+        background-color: #d4c1c1; 
+        float: right;
+    } 
+    
+    /*centre the display image inside the container*/ 
+ 
+    
+    /*set image properties*/ 
+ 
+    
+    /*set padding to the container*/ 
+    .cont { 
+        padding: 10px; 
+    } 
+      
+    /*set the forgot password text*/ 
+    span.psw { 
+        float: right; 
+        padding-top: -5px; 
+    } 
+    
+    /*set the Modal background*/ 
+    .modal { 
+        display: none; 
+        position: fixed; 
+        z-index: 1; 
+        left: 0; 
+        top: 0; 
+        width: 100%; 
+        height: 100%; 
+        overflow: auto; 
+        background-color: rgb(0, 0, 0); 
+        background-color: rgba(0, 0, 0, 0.4); 
+        padding-top: 60px;
+
+
+    } 
+    
+    /*style the model content box*/ 
+    .modal-content { 
+        background-color: #fefefe; 
+        margin: 5% auto 15% auto; 
+        border: 1px solid #888; 
+        width: 30%; 
+        height: 70%;
+    } 
+    
+    /*style the close button*/ 
+    .close { 
+        position: absolute; 
+        right: 25px; 
+        top: 0; 
+        color: #000; 
+        font-size: 35px; 
+        font-weight: bold;
+        float: right; 
+    } 
+      
+    .close:hover, 
+    .close:focus { 
+        color: red; 
+        cursor: pointer; 
+    } 
+    
+    /* add zoom animation*/ 
+    .animate { 
+        -webkit-animation: animatezoom 0.6s; 
+        animation: animatezoom 0.6s 
+    }
+    .wrap {height:100%; overflow:hidden;}
+
+    @-webkit-keyframes animatezoom { 
+        from { 
+            -webkit-transform: scale(0) 
+        } 
+        to { 
+            -webkit-transform: scale(1) 
+        } 
+    } 
+      
+    @keyframes animatezoom { 
+        from { 
+            transform: scale(0) 
+        } 
+        to { 
+            transform: scale(1) 
+        } 
+    } 
+      
+    @media screen and (max-width: 300px) { 
+        span.psw { 
+            display: block; 
+            float: none; 
+        } 
+        .cancelbtn { 
+            width: 100%; 
+        } 
+    } 
+     #map { 
+        height: 400px; 
+        width: 100%; 
+       }
+  </style>
+  </head>
+  <body>
+  	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index1.html">Dabba 4<small>Mumbai</small></a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="#" class="nav-link">Menu</a></li>
+	          <li class="nav-item"><a href="#" class="nav-link">Services</a></li>
+	          <li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
+	          <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+	          
+	          <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+	          <li class="nav-item cart"><a href="#" class="nav-link"><span class="icon icon-shopping_cart"></span><span class="bag d-flex justify-content-center align-items-center"><small>1</small></span></a></li>
+	        </ul>
+	      </div>
+		  </div>
+	  </nav>
+    <!-- END nav -->
+
+    <section class="home-slider owl-carousel">
+      <div class="slider-item" style="background-image: url(burger-1.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
+
+            <div class="col-md-8 col-sm-12 text-center ftco-animate">
+            	<span class="subheading">Welcome</span>
+              <h1 class="mb-4">DabbaWala Service</h1>
+              <p class="mb-4 mb-md-5">Mumbai's Only ECO-FRIENDLY Delivery which brings Home's Love to Your Footstep.<br>Delivering over 50K tiffins daily,</p>
+              <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3" onclick="document.getElementById('id01').style.display='block'">Book Delivery</a></p>
+                  
+  
+    
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+ 
+      <div class="slider-item" style="background-image: url(dessert-2.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
+
+            <div class="col-md-8 col-sm-12 text-center ftco-animate">
+            	<span class="subheading">Welcome</span>
+              <h1 class="mb-4">TIFFIN SERVICE</h1>
+              <p class="mb-4 mb-md-5">Bringing fresh cooked food to yous.</p>
+              <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3" onclick="document.getElementById('id01').style.display='block'">Order Now</a> 
+                <a href="#" class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Menu</a></p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+
+<div id="id01" class="modal"> 
+                      
+                    <form method="POST" class="modal-content animate" action=""> 
+ 
+                      
+                      <div class="cont">
+                        <div class="hand"><p align="center">SIGN UP</p></div><hr>
+                        <input type="text" placeholder="Enter Firstname" name="fname" required>
+                        <input type="text" placeholder="Enter Lastname" name="lname" required><br> 
+                        <input type="text" placeholder="Enter Email id" name="email" required>
+                        <input type="password" placeholder="Enter Password" name="psw1" required><br>
+                        <input type="password" placeholder="Repeat Password" name="psw2" required>
+                        <input type="text" placeholder="Enter Contact no." name="contact" required>
+                         
+                         
+                         
+                      </div> 
+  
+            <div class="cont" style="background-color:#f1f1f1"> 
+               <a href="index1.html"><button type="submit">Signup</button></a>
+                <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button> 
+                <span class="psw">Already Registered? <a id="switchForm" href="#">Login</a></span> 
+            </div> 
+        </form> 
+    </div> 
+
+
+<div id="id02" class="modal"> 
+                      
+                    <form id="login_form" class="modal-content" action="/action_page.php"> 
+ 
+                      
+                      <div class="cont"> 
+                        <div class="hand"><p align="center">SIGN IN</p></div><hr>
+                        <input type="text" placeholder="Enter registered email id" name="lemail" required>
+
+                        <input type="password" placeholder="Enter Password" name="lpsw" required><br> 
+                         
+                         
+                      </div> 
+  
+            <div class="container" style="background-color:#f1f1f1"> 
+                <button type="submit">Login</button>
+                <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button> 
+                <span class="psw">Create Account? <a id="switchForm" href="#">Register</a></span> 
+            </div> 
+        </form> 
+    </div> 
+
+
+
+
+<!-- About Section -->
+<div class="w3-container" style="padding:128px 16px" id="about">
+  <h1 class="w3-center"><b>Why MDS</b></h1>
+  <p class="w3-center w3-large">Learn more about us</p>
+  <div class="w3-row-padding w3-center" style="margin-top:64px">
+    <div class="w3-quarter">
+      <img src="image1.png">
+      <p class="w3-large"><b>Ghar ka Khana</b></p>
+      <p>Delicious Home made food at your doorstep.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="image2.png">
+      <p class="w3-large"><b>Amazing Variety</b></p>
+      <p>You will love the variety coz at MDS - No dish is repeated All Week!.</p>
+    </div>
+    <div class="w3-quarter">
+     <img src="image3.png">
+           <p class="w3-large"><b>Easy Ordering</b></p>
+      <p>Ordering your meal on the MDS! Website will take less than 2 mins!!.</p>
+    </div>
+    <div class="w3-quarter">
+      <img src="image4.png">
+      <p class="w3-large"><b>My Account</b></p>
+      <p>Manage your orders - Cancellations, Order Renewals, Shares etc within seconds.</p>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="contain">
+  <div class="container innerpage">
+    <div class="common_inner">
+      <div class="common_content mb10">
+            Let’s follow these simple steps to help you understand our concept.
+          </div>
+          <div class="row concept-box-parent-row">
+            <div class="col-md-20 col-sm-50 col-xs-100 concept-box-parent mb20">
+              <div class="concept-box">
+                <div class="box-top box_bg1 text-center">
+                  1
+                  <i class="fa fa-spoon spoon-left"></i>
+                  <i class="fa fa-spoon spoon-left spoon-right"></i>
+                </div>
+                <div class="middle-box">
+                Sign up/Register
+                </div>
+                <div class="below_box below_box1"></div>
+              </div>
+            </div>
+            <div class="col-md-20 col-sm-50 col-xs-100 concept-box-parent mb20">
+              <div class="concept-box">
+                <div class="box-top box_bg2 text-center">
+                  2
+                  <i class="fa fa-spoon spoon-left"></i>
+                  <i class="fa fa-spoon spoon-left spoon-right"></i>
+                </div>
+                <div class="middle-box">
+                Pay at your doorstep
+                </div>
+                <div class="below_box below_box2"></div>
+              </div>
+            </div>
+            <div class="col-md-20 col-sm-50 col-xs-100 concept-box-parent mb20">
+              <div class="concept-box">
+                <div class="box-top box_bg1 text-center">
+                  3
+                  <i class="fa fa-spoon spoon-left"></i>
+                  <i class="fa fa-spoon spoon-left spoon-right"></i>
+                </div>
+                <div class="middle-box">
+                  Login
+                </div>
+                <div class="below_box below_box1"></div>
+              </div>
+            </div>
+            <div class="col-md-20 col-sm-50 col-xs-100 concept-box-parent mb20">
+              <div class="concept-box">
+                <div class="box-top box_bg2 text-center">
+                  4
+                  <i class="fa fa-spoon spoon-left"></i>
+                  <i class="fa fa-spoon spoon-left spoon-right"></i>
+                </div>
+                <div class="middle-box">
+                  Customize order 
+                </div>
+                <div class="below_box below_box2"></div>
+              </div>
+            </div>
+            <div class="col-md-20 col-sm-50 col-xs-100 concept-box-parent mb20">
+              <div class="concept-box">
+                <div class="box-top box_bg1 text-center">
+                  5
+                  <i class="fa fa-spoon spoon-left"></i>
+                  <i class="fa fa-spoon spoon-left spoon-right"></i>
+                </div>
+                <div class="middle-box">
+                Tiffin delivered
+                </div>
+                <div class="below_box below_box1"></div>
+              </div>
+            </div>            
+          </div>
+                  </div>
+      </div>
+    </div>
+    <div class="container">
+  <div class="container innerpage">
+    <div class="common_inner">
+          <div class="common_content">
+            <div class="common_content"><b>Your tiffin would be delivered by MDS's certified dabbawalas. Delivery charges per month is between Rs.600-Rs.1000</b></div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+    <p align="center">
+    <font size="12"><b>Support</b>
+    </font>
+  </p>
+   <p align="center">For more info and support,contact us</p><br><t><t><t>
+   <a href="#"><i class="fa fa-envelope-o" style="font-size:15px;color:white"></i> info@gmail.com</a></p>
+   <a href="#"><i class="fa fa-phone" style="font-size:15px;color:white"></i> 9920938994</a>
+</div>
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d120643.5111867835!2d72.80400734326115!3d19.10284176129004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c627a20bcaa9%3A0xb2fd3bcfeac0052a!2sK.+J.+Somaiya+College+of+Engineering!5e0!3m2!1sen!2sin!4v1564905550622!5m2!1sen!2sin" width="1400" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+<footer class="w3-container w3-padding-60 w3-center w3-opacity">  
+  <div class="w3-xlarge w3-padding-32">
+    <p>Find me on social media</p>
+    <a href="#"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+    <a href="#"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+    <a href="#"><i class="fa fa-snapchat w3-hover-opacity"></i></a>
+    <a href="#"><i class="fa fa-pinterest-p w3-hover-opacity"></i></a>
+    <a href="#"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+    <a href="#"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
+ </div>
+ 
+
+<script>
+$("#switchForm").click(function(){
+  const isLoginVisible = $("#login_form").is(':not(:hidden)');
+  if(isLoginVisible) {
+    $("#login_form").hide();
+    $("#register_form").show();
+    $("#switchForm > p").html("Login");
+  } else {
+    $("#login_form").show();
+    $("#register_form").hide();
+    $("#switchForm > p").html("Register");
+  }
+});
+</script>
+
+</footer>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="js/main.js"></script>
+
+  </body>
+</html>
